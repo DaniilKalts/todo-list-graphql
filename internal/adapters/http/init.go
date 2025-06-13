c3e3ee01500b5c39cf59fe0051a25f4f9ff62bd4
DaniilKalts <kaltsdaniil@gmail.com>
2025-06-13 17:54:11 +0500

    feat: add fiber

--- internal/adapters/http/init.go
+++ internal/adapters/http/init.go
@@ -1,49 +1,49 @@
 package http
 
 import (
-	"gorm.io/gorm"
 	"log"
-	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/DaniilKalts/todo-list-graphql/graph"
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
 	"github.com/vektah/gqlparser/v2/ast"
+	"gorm.io/gorm"
 
+	"github.com/DaniilKalts/todo-list-graphql/graph"
 	"github.com/DaniilKalts/todo-list-graphql/internal/config"
 )
 
 func StartServer(cfg *config.Config, db *gorm.DB) error {
 	srv := handler.New(
 		graph.NewExecutableSchema(
-			graph.
-				Config{Resolvers: &graph.Resolver{DB: db}},
+			graph.Config{Resolvers: &graph.Resolver{DB: db}},
 		),
 	)
 
-	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
 	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Options{})
 
 	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
-
 	srv.Use(extension.Introspection{})
-	srv.Use(
-		extension.AutomaticPersistedQuery{
-			Cache: lru.New[string](100),
-		},
-	)
+	srv.Use(extension.AutomaticPersistedQuery{Cache: lru.New[string](100)})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	app := fiber.New()
+	app.All(
+		"/",
+		adaptor.HTTPHandler(
+			playground.Handler("GraphQL Playground", "/query"),
+		),
+	)
+	app.All("/query", adaptor.HTTPHandler(srv))
 
 	log.Printf(
-		"connect to http://localhost:%s/ for GraphQL playground",
+		"→ connect to http://localhost:%s/ for GraphQL playground",
 		cfg.Server.Port,
 	)
-
-	return http.ListenAndServe(":"+cfg.Server.Port, nil)
+	return app.Listen(":" + cfg.Server.Port)
 }
